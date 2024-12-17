a730a0a79862c01e654f8abf9ee3f93d4df50f3a
igr76 <gritsuk766@gmail.com>
2024-12-17 14:34:59 +1000

    start

---
+++ src/main/java/org/example/mqttservic/MqttServicApplication.java
@@ -0,0 +1,13 @@
+package org.example.mqttservic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttServicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttServicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mqttservic/MqttServicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mqttservic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttServicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
